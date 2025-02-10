deb1e39440ddd0ab20f10b3f890c178fc85db89e
Jano-C <jano.cecconi@gmail.com>
2025-02-10 17:53:53 -0300

    home screen implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widget_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,46 @@ class HomeScreen extends StatelessWidget {
         centerTitle: true,
         title: const Text('Home Screen'),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return ListView.builder(
+       itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItems[index];
+        return _CustomLitsTile(menuItem: menuItem);
+        
+      });
+  }
+}
+
+class _CustomLitsTile extends StatelessWidget {
+  final MenuItems menuItem;
+  const _CustomLitsTile({
+    required this.menuItem,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+    return ListTile(
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subtitle),
+      leading: Icon(menuItem.icon, color: colors.primary),
+      onTap: () {
+            print("Click en ${menuItem.title}");
+          },
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
+          
     );
   }
 }
