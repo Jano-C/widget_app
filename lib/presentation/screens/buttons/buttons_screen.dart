86f6af13e1d58cd1c2fae5dea72f390b914fa8cf
Jano-C <jano.cecconi@gmail.com>
2025-02-17 19:49:51 -0300

    mas botones

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -27,12 +27,61 @@ class _ButtonsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-      child: Wrap(
-        children: [
-          
-        ],
+    final color = Theme.of(context).colorScheme;
 
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(onPressed: (){}, child: const Text('Elevated Button')),
+            const ElevatedButton(onPressed: null, child: Text('Elevated Button')),
+            ElevatedButton.icon(onPressed: (){}, label: const Text('Elevated Button'), icon: const Icon(Icons.access_alarm_rounded)),
+            FilledButton(onPressed: (){}, child: Text('hola')),
+            FilledButton.icon(onPressed: (){}, label: Text('data'), icon: Icon(Icons.accessibility_new_rounded)),
+            OutlinedButton(onPressed: (){}, child: Text('data')),
+            OutlinedButton.icon(onPressed: (){}, label: Text('data'), icon: Icon(Icons.terminal_rounded)),
+            TextButton(onPressed: (){}, child: Text('data')),
+            TextButton.icon(onPressed: (){}, label: Text('data'), icon: Icon(Icons.api_sharp)),
+            IconButton(onPressed: (){}, icon: Icon(Icons.app_registration_rounded)),
+            IconButton(onPressed: (){}, icon: Icon(Icons.app_registration_rounded),
+            style: ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(color.primary),
+              
+            )
+            ),
+            const CustomButton(),
+          ],
+
+      
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+    return  ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: (){},
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              
+          child: Text('hola mundo', style: TextStyle(color: Colors.white)) ,
+          ),
+        ),
       ),
     );
   }
