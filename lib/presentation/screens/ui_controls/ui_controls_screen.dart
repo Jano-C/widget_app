d229f49803219dd56f2e93ef9d486f392cfd6174
Jano-C <jano.cecconi@gmail.com>
2025-03-17 20:29:47 -0300

    screens creadas y barril actualizado

---
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ui_controls_screens extends StatelessWidget {
+
+  static const name = 'ui_controls_screen';
+
+  const ui_controls_screens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
